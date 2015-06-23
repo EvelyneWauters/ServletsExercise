b7edea7f95e40afd6f4c335554aee43193568971
EvelyneWauters <evelyne.wauters@gmail.com>
2015-06-23 12:12:43 +0200

    added dependencies & file structure

--- src/main/java/Main.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Evelyne on 23/06/15.
- */
-public class Main {
-}
---
+++ src/main/java/be/vdab/HelloServlet.java
@@ -0,0 +1,9 @@
+package be.vdab;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by Evelyne on 23/06/15.
+ */
+public class HelloServlet extends HttpServlet {
+}
